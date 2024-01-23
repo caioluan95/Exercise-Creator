951de5a92e8db3027c2d8a3968f1e8ce4f7e5d81
ST Distribuidora <caioluan18@gmail.com>
2024-01-23 20:06:17 -0300

    fixed the logo problem

--- src/core/components/form/inputs/input-search/index.jsx
+++ src/core/components/form/inputs/input-search/index.jsx
@@ -3,7 +3,7 @@ import { IoMdSearch } from "react-icons/io";
 export default function InputSearch() {
   return (
     <div>
-      <div className="flex md:order-2">
+      <div className="flex md:order-2 w-48">
         <div className="relative md:block">
           <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
             <IoMdSearch classNameName="text-gray-500" />
@@ -13,7 +13,7 @@ export default function InputSearch() {
           <input
             type="text"
             id="search-navbar"
-            className="block w-full min-w-32 p-2 ps-10 text-sm text-gray-400 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 bg-sky-100"
+            className="block w-full p-2 ps-10 text-sm text-gray-400 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 bg-sky-100"
             placeholder="Search..."
           />
         </div>
